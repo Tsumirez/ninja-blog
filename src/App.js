3a33333962f8ca7bc5b6ead18c1e25f6e83d89ef
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-25 16:32:52 +0200

    Simple React tutorial with Net Ninja. Initial commit.
    Kinda did a bit 'fore I caught on i should "git it".
    
    React short how-to:
    With nodejs installed, just type:
    npx create-react-app name-of-your-app-here
    
    Index html is a static file that will be injected with react components. Just leave it alone,
    unless you plan some general template edits.
    
    index.js is the main js file that loads the whole app into index.html's "root" div.
    App. js is the file that contains top most app component. By editing this page i changed it's contents.
    
    React components are functions that return html-like jsx code. This code is transpiled by Babel to real html and javascript.
    Notice how instead of using html's class attribute we use className. that's because we're still in javascript and class is a
    reserved keyword here.
    
    If you put something in {} inside jsx component's "html" it'll be treated as javascript code to be executed
    That way you can insert dynamic values as shown in code. Be they references to variables or expressions they will work.
    Notice that in a link to gw2 website, the href attribute is not quoted when passed in as a js variable value.
    That is another difference in jsx from standard template approach. Just the dynamic value, no need to wrap it in quotes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = "Welcome on the chopping block"
+  let dislikes = 50;
+  const person = {name: 'Zef', age: 40}
+  const link="http://www.guildwars2.com"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="content">
+       <h1>{title}</h1>
+       <p>Likes: -{dislikes}</p>
+       <p>Mostly by {"hello ninjas"}</p>
+       <p>{[0,1,2,3,4]}</p>
+       <p>{Math.random()*10}</p>
+
+       <a href={link}>Link to awesome</a>
+     </div>
     </div>
   );
 }
