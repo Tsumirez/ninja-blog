3f3e03f2f3afd5097e73d89781bbc96289d7176a
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-25 21:25:22 +0200

    Added new components and removed component-based css. All css is now in global index.css file
    Well..except one inline style in Navbar.js
    Notice the use of double curly brackets. One pair is for containing js code.
    The other is for creating object literal that contains css properties.
    Keep in mind it's still javascript so we're using javascript version of css property names and values,
    such as backgroundColor (js) and not background-color (css).

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import './App.css';
+import NavBar from './Navbar'
+import Home from './Home'
 
 function App() {
 
-  const title = "Welcome on the chopping block"
-  let dislikes = 50;
-  const person = {name: 'Zef', age: 40}
-  const link="http://www.guildwars2.com"
-
   return (
     <div className="App">
+     <NavBar />
      <div className="content">
-       <h1>{title}</h1>
-       <p>Likes: -{dislikes}</p>
-       <p>Mostly by {"hello ninjas"}</p>
-       <p>{[0,1,2,3,4]}</p>
-       <p>{Math.random()*10}</p>
-
-       <a href={link}>Link to awesome</a>
+       <Home />
      </div>
     </div>
   );
