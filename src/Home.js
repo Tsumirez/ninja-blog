18856b74360eadd396f131fc9ceaaf0f3352b9b1
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-25 23:44:15 +0200

    Reacting to click events
    Simple event handling no biggie.
    One cool thing is bit of inline factory pattern to call function with arguments.
    Just create annonymous function for the onClick handler that returns the desired one with parameters passed.
    Nice and easy:3

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = (e) => console.log('ninja button pressed!', e)
+    const handleClickAgain = (name,e) => console.log(`Ninjas know your name is ${name}..and other stuff such as..`, e.target)
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Zef',e)}>Another clickbait</button>
         </div>
     )
 }
