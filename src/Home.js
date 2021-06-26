3a02a5202b6c75f2e798a1348246dc50096e14a8
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-26 23:00:47 +0200

    Reusing a component.
    Like in last commit, but this time in practice.
    BlogList is now reused in home component for a second list of blogs.
    By using filter method of array, we can filter blogs array to only include those
    that meet specified cryteria.
    
    Filter function runs each item in array against a logical condition.
    If condition is true the item is kept, if false it's discarded.
    The resulting new array is returned as result.
    
    Here i made the second list include all blogs but those written by Zef.

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,7 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All blogs" />
+            <BlogList blogs={blogs.filter(blog => blog.author!=='Zef')} title="Blogs not from Zef" />
         </div>
     )
 }
