b44e81faaaa98d9b4a83476a18b095510c576965
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-28 11:31:43 +0200

    Added error handling
    The request from server might fail. For that error handling is required.
    There some issues that needed addressing:
    
    1. catching error of failed connection to server.
    2. catching errors when server connection and response work proper, but there's an error fetching the resource
    3. making UI respond properly to an error (such as terminating loading.. message and ensuring old results do not stay on screen
    when attempting to fetch new data ended with error)
    
    Notice that when using fetch .then() syntax, errors are intercepted with .catch(err) at end, not traditional
    try .. catch blocks.
    
    Also removed some old console log lines for testing from earlier lessons.

--- src/Home.js
+++ src/Home.js
@@ -5,26 +5,35 @@ const Home = () => {
     //let name = 'Zef'
     const [blogs, setBlogs] = useState(null);
     const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
 
     useEffect(()=>{
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    setIsPending(false);
+                    setBlogs(null);
+                    throw Error('Just as you thought - it\'s fucked.')
+                }
                 return res.json()
             })
-            .then((data => {
-                console.log(data)
+            .then(data => {
                 setBlogs(data);
                 setIsPending(false);
-            }))
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+            })
         },1000);
     },[])
 
     return (
         <div className="home">
+            {error  && <div>Error: {error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs" />}
-            {/* <BlogList blogs={blogs.filter(blog => blog.author!=='Zef')} title="Blogs not from Zef" /> */}
         </div>
     )
 }
