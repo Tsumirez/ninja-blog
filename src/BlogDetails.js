5abcd00bce73669bd461b66fb4cccabb822f1631
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-29 20:17:19 +0200

    Reusing custom hook
    Simple story here. useFetch is reused for BlogDetails component to display the blog.
    It works.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,19 @@
 import { useParams } from "react-router";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id} </h2>
+            {isPending && '<p>Loading ... </p>'} 
+            {error && <div>{error}</div>}
+            <article>
+              <h2>{blog.title}</h2>
+              <h4>Author: {blog.author}</h4>
+              <div>{blog.body}</div>
+            </article>
         </div>
     )
 }
