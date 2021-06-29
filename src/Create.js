98a53a30193eb9dd22aa55eca505ab3f80ab8f89
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-29 22:09:23 +0200

    React form fields
    You can link state of a component with form fields.
    However this must be a two way binding. If you just set the value of form fields
    to state's value, it'll become unchangeable.
    
    You must provide an onChange function that will update the state with form field's value.
    
    const [title,setTitle] = useState('');
    
    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)}/>
    
    And that would be that all fine and good...
    Except i'm a nosy ass and like to complicate things.
    Seems the "set" state function is asynchronous.
    Adding a line of console.log after it, that outputs "new" title value actually outputs old one.
    The change does happen - output it to html element and it shows it just fine.
    But console gets the old value despite the log statement being made after the assignement of field value as new state..

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+
 const Create = () => {
-    return (
-        <div className="create">
-            <h2>Add a new Blog</h2>
-        </div>
-    );
-}
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('Zef')
+
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required value={title} 
+         onChange={(e)=> {
+             setTitle(e.target.value)
+         }}/>
+
+        <label>Blog body:</label>
+        <textarea required onChange={
+            (e) => setBody(e.target.value)
+        }></textarea>
+        
+        <label>Blog author:</label>
+        <select value={author} onChange={(e)=> setAuthor(e.target.value)}>
+          <option value="Zef">Zef</option>
+          <option value="Tsumirez">Tsumirez</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}, {body}, {author}</p>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
