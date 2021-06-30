3bdcd4f42133c49739c0a30b05356b7d856b9c3f
zefirez@zoho.com <tsumirez@zoho.com>
2021-06-30 07:19:43 +0200

    useHistory hook and url redirecting
    useHistory hook allows for redirecting to different url, as well as navigating page history.
    Here we'll use it to redirect user to main blog page once his new blog has been successfully submitted and added.
    
    import { useHistory } from 'react-router-dom' - this hook is part or react router
    
    const history = useHistory(); and that's how you intitiate it.
    
    history.go(-2) - this is how you browse history. Number defines steps in history from current spot.
    Negative number goes back, positive forward.
    
    history.push('/') - and this is the url redirection. It goes to designed url.

--- src/Create.js
+++ src/Create.js
@@ -1,45 +1,52 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [author, setAuthor] = useState('Zef')
-    const[isPending, setIsPending] = useState(false)
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Zef");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = {title, body, author};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
-        setIsPending(true);
+    setIsPending(true);
 
-        fetch('http://localhost:8000/blogs',{
-          method: 'POST', 
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(blog)
-        })
-        .then(() => {
-          console.log('new blog added')
-          setIsPending(false)
-        })
-    }
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+      history.push('/');
+    });
+  };
+
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
       <form onSubmit={handleSubmit}>
-          
         <label>Blog title:</label>
-        <input type="text" required value={title} 
-         onChange={(e)=> {
-             setTitle(e.target.value)
-         }}/>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
 
         <label>Blog body:</label>
-        <textarea required value={body} onChange={
-            (e) => setBody(e.target.value)
-        }></textarea>
-        
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
         <label>Blog author:</label>
-        <select value={author} onChange={(e)=> setAuthor(e.target.value)}>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="Zef">Zef</option>
           <option value="Tsumirez">Tsumirez</option>
         </select>
